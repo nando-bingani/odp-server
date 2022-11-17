0371c71af563f9276c78d6063fd939315148fcf7
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-11-17 07:39:48 +0200

    Document auth tests and cover edge cases
    Roles and clients that are not flagged as collection-specific should behave as such regardless of any collection associations.

--- test/test_auth.py
+++ test/test_auth.py
@@ -3,6 +3,8 @@ from test.factories import ClientFactory, CollectionFactory, RoleFactory, ScopeF
 
 
 def assert_compare(expected: Permissions, actual: Permissions):
+    """Compare expected and actual permissions results. The order of
+    collection ids does not matter."""
     if expected == actual:
         return
 
@@ -18,10 +20,18 @@ def assert_compare(expected: Permissions, actual: Permissions):
 
 
 def test_platform_roles():
+    """Test permissions calculations for 'platform' (not collection-specific) roles.
+
+    The client is not collection-specific. Any client-collection and role-collection
+    associations should be ignored.
+
+    The expected user permissions result contains the intersection of client and
+    role scopes, with a value of '*' for each scope.
+    """
     scopes = ScopeFactory.create_batch(8, type='odp')
-    client = ClientFactory(scopes=scopes[1:7], collection_specific=False)
-    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False)
-    role2 = RoleFactory(scopes=scopes[5:], collection_specific=False)
+    client = ClientFactory(scopes=scopes[1:7], collection_specific=False, collections=[CollectionFactory()])
+    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False, collections=[CollectionFactory()])
+    role2 = RoleFactory(scopes=scopes[5:], collection_specific=False, collections=[CollectionFactory()])
     user = UserFactory(roles=(role1, role2))
 
     actual_user_perm = get_user_permissions(user.id, client.id)
@@ -41,8 +51,17 @@ def test_platform_roles():
 
 
 def test_collection_roles():
+    """Test permissions calculations for collection-specific roles.
+
+    The client is not collection-specific. Any client-collection associations
+    should be ignored.
+
+    The expected user permissions result contains the intersection of client and
+    role scopes, with the value for each scope being the set of applicable role
+    collections.
+    """
     scopes = ScopeFactory.create_batch(8, type='odp')
-    client = ClientFactory(scopes=scopes[1:7], collection_specific=False)
+    client = ClientFactory(scopes=scopes[1:7], collection_specific=False, collections=[CollectionFactory()])
     role1 = RoleFactory(scopes=scopes[:5], collection_specific=True, collections=CollectionFactory.create_batch(3))
     role2 = RoleFactory(scopes=scopes[3:], collection_specific=True, collections=CollectionFactory.create_batch(3))
     user = UserFactory(roles=(role1, role2))
@@ -72,9 +91,19 @@ def test_collection_roles():
 
 
 def test_platform_collection_role_mix():
+    """Test permissions calculations for a mix of platform and collection-specific roles.
+
+    The client is not collection-specific. Any client-collection associations - and
+    role-collection associations for platform roles - should be ignored.
+
+    The expected user permissions result contains the intersection of client and
+    role scopes, with a value of '*' for scopes allowed by platform roles, and the
+    set of applicable role collections for remaining scopes allowed by collection-specific
+    roles.
+    """
     scopes = ScopeFactory.create_batch(8, type='odp')
-    client = ClientFactory(scopes=scopes[1:7], collection_specific=False)
-    role1 = RoleFactory(scopes=scopes[:4], collection_specific=False)
+    client = ClientFactory(scopes=scopes[1:7], collection_specific=False, collections=[CollectionFactory()])
+    role1 = RoleFactory(scopes=scopes[:4], collection_specific=False, collections=[CollectionFactory()])
     role2 = RoleFactory(scopes=scopes[3:], collection_specific=True, collections=CollectionFactory.create_batch(3))
     role3 = RoleFactory(scopes=scopes[5:], collection_specific=True, collections=CollectionFactory.create_batch(3))
     user = UserFactory(roles=(role1, role2, role3))
@@ -104,10 +133,18 @@ def test_platform_collection_role_mix():
 
 
 def test_collection_client():
+    """Test permissions calculations for a collection-specific client.
+
+    The roles are not collection-specific. Any role-collection associations
+    should be ignored.
+
+    The expected user permissions result contains the intersection of client and
+    role scopes, with the value for each scope being the set of client collections.
+    """
     scopes = ScopeFactory.create_batch(8, type='odp')
     client = ClientFactory(scopes=scopes[1:7], collection_specific=True, collections=CollectionFactory.create_batch(3))
-    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False)
-    role2 = RoleFactory(scopes=scopes[5:], collection_specific=False)
+    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False, collections=[CollectionFactory()])
+    role2 = RoleFactory(scopes=scopes[5:], collection_specific=False, collections=[CollectionFactory()])
     user = UserFactory(roles=(role1, role2))
 
     actual_user_perm = get_user_permissions(user.id, client.id)
@@ -127,11 +164,22 @@ def test_collection_client():
 
 
 def test_collection_client_platform_collection_role_mix():
+    """Test permissions calculations for a collection-specific client, with a mix
+    of platform and collection-specific roles.
+
+    Any role-collection associations for platform roles should be ignored.
+
+    The expected user permissions result contains the intersection of client and
+    role scopes, with the value for each scope being the set of client collections
+    for scopes allowed by platform roles, and the intersection of client and role
+    collections for remaining scopes allowed by collection-specific roles. If the
+    latter intersection is empty, the scope should not appear in the result.
+    """
     scopes = ScopeFactory.create_batch(8, type='odp')
     client = ClientFactory(scopes=scopes[1:7], collection_specific=True, collections=CollectionFactory.create_batch(3))
-    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False)
+    role1 = RoleFactory(scopes=scopes[:3], collection_specific=False, collections=[CollectionFactory()])
     role2 = RoleFactory(scopes=scopes[3:5], collection_specific=True, collections=CollectionFactory.create_batch(3))
-    role3 = RoleFactory(scopes=scopes[5:], collection_specific=True, collections=client.collections[1:])
+    role3 = RoleFactory(scopes=scopes[5:], collection_specific=True, collections=client.collections[1:] + [CollectionFactory()])
     user = UserFactory(roles=(role1, role2, role3))
 
     actual_user_perm = get_user_permissions(user.id, client.id)
@@ -155,9 +203,10 @@ def test_collection_client_platform_collection_role_mix():
 
 
 def test_user_info():
-    client = ClientFactory(collection_specific=False)
-    role1 = RoleFactory(collection_specific=False)
-    role2 = RoleFactory(collection_specific=False)
+    """Test user info result with non-collection-specific client and roles."""
+    client = ClientFactory(collection_specific=False, collections=[CollectionFactory()])
+    role1 = RoleFactory(collection_specific=False, collections=[CollectionFactory()])
+    role2 = RoleFactory(collection_specific=False, collections=[CollectionFactory()])
     user = UserFactory(roles=(role1, role2))
 
     actual_user_info = get_user_info(user.id, client.id)
@@ -173,8 +222,10 @@ def test_user_info():
 
 
 def test_user_info_collection_roles():
-    client = ClientFactory(collection_specific=False)
-    role1 = RoleFactory(collection_specific=False)
+    """Test user info result with a non-collection-specific client and a mix
+    of platform and collection-specific roles."""
+    client = ClientFactory(collection_specific=False, collections=[CollectionFactory()])
+    role1 = RoleFactory(collection_specific=False, collections=[CollectionFactory()])
     role2 = RoleFactory(collection_specific=True, collections=CollectionFactory.create_batch(3))
     role3 = RoleFactory(collection_specific=True, collections=CollectionFactory.create_batch(3))
     user = UserFactory(roles=(role1, role2, role3))
@@ -192,8 +243,14 @@ def test_user_info_collection_roles():
 
 
 def test_user_info_collection_roles_and_client():
+    """Test user info result with a collection-specific client and a mix
+    of platform and collection-specific roles.
+
+    Collection-specific roles that do not share any collections with the
+    client should not appear in the user's role list.
+    """
     client = ClientFactory(collection_specific=True, collections=CollectionFactory.create_batch(3))
-    role1 = RoleFactory(collection_specific=False)
+    role1 = RoleFactory(collection_specific=False, collections=[CollectionFactory()])
     role2 = RoleFactory(collection_specific=True, collections=client.collections[:2])
     role3 = RoleFactory(collection_specific=True, collections=client.collections[2:])
     role4 = RoleFactory(collection_specific=True, collections=CollectionFactory.create_batch(3))
