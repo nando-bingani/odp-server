3fdb634bd56282a633da816926d52da188c07eff
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2022-12-19 10:10:43 +0200

    Avoid loading indexing data when querying catalog records

--- odp/db/models/catalog_record.py
+++ odp/db/models/catalog_record.py
@@ -1,6 +1,6 @@
 from sqlalchemy import ARRAY, Boolean, Column, ForeignKey, Integer, Numeric, String, TIMESTAMP
 from sqlalchemy.dialects.postgresql import JSONB, TSVECTOR
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import deferred, relationship
 
 from odp.db import Base
 
@@ -29,11 +29,11 @@ class CatalogRecord(Base):
     error_count = Column(Integer)
 
     # internal catalog indexing
-    full_text = Column(TSVECTOR)
-    keywords = Column(ARRAY(String))
-    spatial_north = Column(Numeric)
-    spatial_east = Column(Numeric)
-    spatial_south = Column(Numeric)
-    spatial_west = Column(Numeric)
-    temporal_start = Column(TIMESTAMP(timezone=True))
-    temporal_end = Column(TIMESTAMP(timezone=True))
+    full_text = deferred(Column(TSVECTOR))
+    keywords = deferred(Column(ARRAY(String)))
+    spatial_north = deferred(Column(Numeric))
+    spatial_east = deferred(Column(Numeric))
+    spatial_south = deferred(Column(Numeric))
+    spatial_west = deferred(Column(Numeric))
+    temporal_start = deferred(Column(TIMESTAMP(timezone=True)))
+    temporal_end = deferred(Column(TIMESTAMP(timezone=True)))
