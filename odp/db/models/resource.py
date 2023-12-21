9b6031a71ae61fc18a0bbbe3d12d2c9631c668b7
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2023-12-21 16:59:01 +0200

    Associate a resource with its provider

--- odp/db/models/resource.py
+++ odp/db/models/resource.py
@@ -11,6 +11,10 @@ class Resource(Base):
     """A reference to a file, folder or web page in an archive.
 
     `path` is relative to `url` of the archive.
+
+    Note the preclusion of foreign key delete cascades:
+    If the containing archive or the resource provider must be deleted,
+    then the resource must either be moved or explicitly deleted first.
     """
 
     __tablename__ = 'resource'
@@ -23,7 +27,10 @@ class Resource(Base):
     md5 = Column(String)
     timestamp = Column(TIMESTAMP(timezone=True))
 
-    archive_id = Column(String, ForeignKey('archive.id', ondelete='CASCADE'), nullable=False)
+    archive_id = Column(String, ForeignKey('archive.id', ondelete='RESTRICT'), nullable=False)
     archive = relationship('Archive')
 
-    _repr_ = 'id', 'path', 'name', 'type', 'size', 'archive_id'
+    provider_id = Column(String, ForeignKey('provider.id', ondelete='RESTRICT'), nullable=False)
+    provider = relationship('Provider')
+
+    _repr_ = 'id', 'path', 'name', 'type', 'size', 'archive_id', 'provider_id'
