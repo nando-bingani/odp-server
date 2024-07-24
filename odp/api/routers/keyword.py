3ee43810cba39b8b97e044b298736c32ede46c9e
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-07-24 13:45:08 +0200

    Update API usage of keyword ids and parent ids
    The keyword regular expression has been removed. The only keyword format requirement - which is enforced in the DB layer - is that a keyword is formed by a non-empty suffix to its parent keyword. This allows us the freedom to structure keywords in whatever way is most appropriate for a vocabulary.

--- odp/api/routers/keyword.py
+++ odp/api/routers/keyword.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timezone
-from functools import partial
 
 from fastapi import APIRouter, Depends, HTTPException, Path, Query
 from jschon import JSON, URI
@@ -10,22 +9,13 @@ from starlette.status import HTTP_404_NOT_FOUND, HTTP_409_CONFLICT, HTTP_422_UNP
 from odp.api.lib.auth import Authorize, Authorized
 from odp.api.lib.paging import Paginator
 from odp.api.models import KeywordHierarchyModel, KeywordModel, KeywordModelAdmin, KeywordModelIn, Page
-from odp.const import KEYWORD_REGEX, ODPScope
+from odp.const import ODPScope
 from odp.const.db import AuditCommand, KeywordStatus, SchemaType
 from odp.db import Session
 from odp.db.models import Keyword, KeywordAudit, Schema
 from odp.lib.schema import schema_catalog
 
 router = APIRouter()
-
-
-def get_parent_key(key: str) -> str:
-    if not (parent_key := key.rpartition('/')[0]):
-        raise HTTPException(
-            HTTP_422_UNPROCESSABLE_ENTITY, 'key must be suffixed to a parent key'
-        )
-
-    return parent_key
 
 
 def get_child_schema(parent: Keyword) -> Schema | None:
@@ -39,16 +29,18 @@ def get_child_schema(parent: Keyword) -> Schema | None:
     return get_child_schema(parent.parent)
 
 
-async def validate_keyword_data(
+async def validate_keyword_input(
+        keyword_id: str,
         keyword_in: KeywordModelIn,
-        key: str,
-        parent_key: str = Depends(get_parent_key),
 ) -> None:
-    if keyword := Session.get(Keyword, key):
-        parent = keyword.parent
-    elif not (parent := Session.get(Keyword, parent_key)):
+    if not keyword_id.startswith(keyword_in.parent_id) or keyword_id == keyword_in.parent_id:
         raise HTTPException(
-            HTTP_404_NOT_FOUND, f"Parent keyword '{parent_key}' does not exist"
+            HTTP_422_UNPROCESSABLE_ENTITY, f"'{keyword_in.parent_id}' cannot be a parent of '{keyword_id}'"
+        )
+
+    if not (parent := Session.get(Keyword, keyword_in.parent_id)):
+        raise HTTPException(
+            HTTP_404_NOT_FOUND, f"Parent keyword '{keyword_in.parent_id}' does not exist"
         )
 
     keyword_jsonschema = schema_catalog.get_schema(URI(
@@ -70,9 +62,10 @@ def output_keyword_model(
 
     schema = get_child_schema(keyword.parent)
     kwargs = dict(
-        key=keyword.key,
+        id=keyword.id,
         data=keyword.data,
         status=keyword.status,
+        parent_id=keyword.parent_id,
         schema_id=schema.id if schema else None,
     )
 
@@ -98,9 +91,10 @@ def create_audit_record(
         user_id=auth.user_id,
         command=command,
         timestamp=timestamp,
-        _key=keyword.key,
+        _id=keyword.id,
         _data=keyword.data,
         _status=keyword.status,
+        _parent_id=keyword.parent_id,
         _child_schema_id=keyword.child_schema_id,
     ).save()
 
@@ -110,14 +104,14 @@ def create_audit_record(
     dependencies=[Depends(Authorize(ODPScope.KEYWORD_READ))],
 )
 async def list_vocabularies(
-        paginator: Paginator = Depends(partial(Paginator, sort='key')),
+        paginator: Paginator = Depends(),
 ) -> Page[KeywordModel]:
     """
     List top-level keywords (root vocabularies). Requires scope `odp.keyword:read`.
     """
     stmt = (
         select(Keyword).
-        where(Keyword.parent_key == None)
+        where(Keyword.parent_id == None)
     )
 
     return paginator.paginate(
@@ -127,25 +121,25 @@ async def list_vocabularies(
 
 
 @router.get(
-    '/{parent_key:path}/',
+    '/{parent_id:path}/',
     dependencies=[Depends(Authorize(ODPScope.KEYWORD_READ))],
 )
 async def list_keywords(
-        parent_key: str = Path(..., title='Parent keyword (vocabulary) identifier', regex=KEYWORD_REGEX),
+        parent_id: str = Path(..., title='Parent keyword (vocabulary) identifier'),
         recurse: bool = Query(False, title='Populate child keywords, recursively'),
-        paginator: Paginator = Depends(partial(Paginator, sort='key')),
+        paginator: Paginator = Depends(),
 ) -> Page[KeywordHierarchyModel] | Page[KeywordModel]:
     """
     List the keywords in a vocabulary. Requires scope `odp.keyword:read`.
     """
-    if not Session.get(Keyword, parent_key):
+    if not Session.get(Keyword, parent_id):
         raise HTTPException(
-            HTTP_404_NOT_FOUND, f"Parent keyword '{parent_key}' does not exist"
+            HTTP_404_NOT_FOUND, f"Parent keyword '{parent_id}' does not exist"
         )
 
     stmt = (
         select(Keyword).
-        where(Keyword.parent_key == parent_key)
+        where(Keyword.parent_id == parent_id)
     )
 
     return paginator.paginate(
@@ -155,47 +149,46 @@ async def list_keywords(
 
 
 @router.get(
-    '/{key:path}',
+    '/{keyword_id:path}',
     dependencies=[Depends(Authorize(ODPScope.KEYWORD_READ))],
 )
 async def get_keyword(
-        key: str = Path(..., title='Keyword identifier', regex=KEYWORD_REGEX),
+        keyword_id: str = Path(..., title='Keyword identifier'),
         recurse: bool = Query(False, title='Populate child keywords, recursively'),
 ) -> KeywordHierarchyModel | KeywordModel:
     """
     Get a keyword. Requires scope `odp.keyword:read`.
     """
-    if not (keyword := Session.get(Keyword, key)):
+    if not (keyword := Session.get(Keyword, keyword_id)):
         raise HTTPException(
-            HTTP_404_NOT_FOUND, f"Keyword '{key}' does not exist"
+            HTTP_404_NOT_FOUND, f"Keyword '{keyword_id}' does not exist"
         )
 
     return output_keyword_model(keyword, recurse)
 
 
 @router.post(
-    '/{key:path}',
+    '/{keyword_id:path}',
 )
 async def suggest_keyword(
         keyword_in: KeywordModelIn,
-        key: str = Path(..., title='Keyword identifier', regex=KEYWORD_REGEX),
+        keyword_id: str = Path(..., title='Keyword identifier'),
         auth: Authorized = Depends(Authorize(ODPScope.KEYWORD_SUGGEST)),
-        parent_key: str = Depends(get_parent_key),
-        _=Depends(validate_keyword_data),
+        _=Depends(validate_keyword_input),
 ) -> KeywordModel:
     """
     Create a keyword with status `proposed`. Requires scope `odp.keyword:suggest`.
     """
-    if Session.get(Keyword, key):
+    if Session.get(Keyword, keyword_id):
         raise HTTPException(
-            HTTP_409_CONFLICT, f"Keyword '{key}' already exists"
+            HTTP_409_CONFLICT, f"Keyword '{keyword_id}' already exists"
         )
 
     keyword = Keyword(
-        key=key,
+        id=keyword_id,
         data=keyword_in.data,
         status=KeywordStatus.proposed,
-        parent_key=parent_key,
+        parent_id=keyword_in.parent_id,
     )
     keyword.save()
 
@@ -210,23 +203,25 @@ async def suggest_keyword(
 
 
 @router.put(
-    '/{key:path}',
+    '/{keyword_id:path}',
 )
 async def set_keyword(
         keyword_in: KeywordModelAdmin,
-        key: str = Path(..., title='Keyword identifier', regex=KEYWORD_REGEX),
+        keyword_id: str = Path(..., title='Keyword identifier'),
         auth: Authorized = Depends(Authorize(ODPScope.KEYWORD_ADMIN)),
-        parent_key: str = Depends(get_parent_key),
-        _=Depends(validate_keyword_data),
+        _=Depends(validate_keyword_input),
 ) -> KeywordModel:
     """
     Create or update a keyword. Requires scope `odp.keyword:admin`.
     """
-    if keyword := Session.get(Keyword, key):
+    if keyword := Session.get(Keyword, keyword_id):
         command = AuditCommand.update
     else:
-        keyword = Keyword(key=key, parent_key=parent_key)
         command = AuditCommand.insert
+        keyword = Keyword(
+            id=keyword_id,
+            parent_id=keyword_in.parent_id,
+        )
 
     if (
             keyword.data != keyword_in.data or
@@ -251,18 +246,18 @@ async def set_keyword(
 
 
 @router.delete(
-    '/{key:path}',
+    '/{keyword_id:path}',
 )
 async def delete_keyword(
-        key: str = Path(..., title='Keyword identifier', regex=KEYWORD_REGEX),
+        keyword_id: str = Path(..., title='Keyword identifier'),
         auth: Authorized = Depends(Authorize(ODPScope.KEYWORD_ADMIN)),
 ) -> None:
     """
     Delete a keyword. Requires scope `odp.keyword:admin`.
     """
-    if not (keyword := Session.get(Keyword, key)):
+    if not (keyword := Session.get(Keyword, keyword_id)):
         raise HTTPException(
-            HTTP_404_NOT_FOUND, f"Keyword '{key}' does not exist"
+            HTTP_404_NOT_FOUND, f"Keyword '{keyword_id}' does not exist"
         )
 
     create_audit_record(
@@ -277,5 +272,5 @@ async def delete_keyword(
 
     except IntegrityError as e:
         raise HTTPException(
-            HTTP_422_UNPROCESSABLE_ENTITY, f"Keyword '{key}' cannot be deleted as it has sub-keywords"
+            HTTP_422_UNPROCESSABLE_ENTITY, f"Keyword '{keyword_id}' cannot be deleted as it has sub-keywords"
         ) from e
--- test/api/test_keyword.py
+++ test/api/test_keyword.py
@@ -32,15 +32,15 @@ def keyword_build(**attr):
 def assert_db_state(keywords_flat):
     """Verify that the keyword table contains the given keyword batch."""
     result = TestSession.execute(select(Keyword)).scalars().all()
-    result.sort(key=lambda k: k.key)
-    keywords_flat.sort(key=lambda k: k.key)
+    result.sort(key=lambda k: k.id)
+    keywords_flat.sort(key=lambda k: k.id)
     assert len(result) == len(keywords_flat)
     for n, row in enumerate(result):
         kw = keywords_flat[n]
-        assert row.key == kw.key
+        assert row.id == kw.id
         assert row.data == kw.data
         assert row.status == kw.status
-        assert row.parent_key == kw.parent_key
+        assert row.parent_id == kw.parent_id
         assert row.child_schema_id == kw.child_schema_id
         assert row.child_schema_type == ('keyword' if kw.child_schema_id else None)
 
@@ -54,9 +54,10 @@ def assert_audit_log(grant_type, *entries):
         assert row.command == entries[n]['command']
         assert_new_timestamp(row.timestamp)
         keyword = entries[n]['keyword']
-        assert row._key == keyword.key
+        assert row._id == keyword.id
         assert row._data == keyword.data
         assert row._status == keyword.status
+        assert row._parent_id == keyword.parent_id
         assert row._child_schema_id == keyword.child_schema_id
 
 
@@ -67,15 +68,16 @@ def assert_no_audit_log():
 def assert_json_result(response, json, keyword, recurse=False):
     """Verify that the API result matches the given keyword object."""
     assert response.status_code == 200
-    assert json['key'] == keyword.key
+    assert json['id'] == keyword.id
     assert json['data'] == keyword.data
     assert json['status'] == keyword.status
+    assert json['parent_id'] == keyword.parent_id
 
     # we have to get the parent using TestSession (rather than FactorySession)
     # because the parent's schema might have changed in an earlier iteration
     # in test_set_keyword
     schema = None
-    parent = TestSession.get(Keyword, keyword.parent_key)
+    parent = TestSession.get(Keyword, keyword.parent_id)
     while parent is not None:
         if schema := parent.child_schema:
             break
@@ -96,8 +98,8 @@ def assert_json_results(response, json, keywords, recurse=False):
     """Verify that the API result list matches the given keyword batch."""
     items = json['items']
     assert json['total'] == len(items) == len(keywords)
-    items.sort(key=lambda i: i['key'])
-    keywords.sort(key=lambda k: k.key)
+    items.sort(key=lambda i: i['id'])
+    keywords.sort(key=lambda k: k.id)
     for n, keyword in enumerate(keywords):
         assert_json_result(response, items[n], keyword, recurse)
 
@@ -136,8 +138,8 @@ def test_list_keywords(
 
     for n in range(4):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.get(f'/keyword/{kw.key}/?recurse={recurse}')
+            if kw.id.count('/') == n:
+                r = client.get(f'/keyword/{kw.id}/?recurse={recurse}')
                 if authorized:
                     assert_json_results(r, r.json(), kw.children, recurse)
                 else:
@@ -168,10 +170,10 @@ def test_list_keywords_not_found(
 
     for n in range(2):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.get(f'/keyword/{kw.key}/foo/?recurse={recurse}')
+            if kw.id.count('/') == n:
+                r = client.get(f'/keyword/{kw.id}/foo/?recurse={recurse}')
                 if authorized:
-                    assert_not_found(r, f"Parent keyword '{kw.key}/foo' does not exist")
+                    assert_not_found(r, f"Parent keyword '{kw.id}/foo' does not exist")
                 else:
                     assert_forbidden(r)
                 break
@@ -194,8 +196,8 @@ def test_get_keyword(
 
     for n in range(4):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.get(f'/keyword/{kw.key}?recurse={recurse}')
+            if kw.id.count('/') == n:
+                r = client.get(f'/keyword/{kw.id}?recurse={recurse}')
                 if authorized:
                     assert_json_result(r, r.json(), kw, recurse)
                 else:
@@ -226,10 +228,10 @@ def test_get_keyword_not_found(
 
     for n in range(2):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.get(f'/keyword/{kw.key}/foo?recurse={recurse}')
+            if kw.id.count('/') == n:
+                r = client.get(f'/keyword/{kw.id}/foo?recurse={recurse}')
                 if authorized:
-                    assert_not_found(r, f"Keyword '{kw.key}/foo' does not exist")
+                    assert_not_found(r, f"Keyword '{kw.id}/foo' does not exist")
                 else:
                     assert_forbidden(r)
                 break
@@ -249,26 +251,28 @@ def test_suggest_keyword(
     client = api(scopes)
 
     keyword_1 = keyword_build(
-        parent_key=keywords_top[2].key,
+        parent_id=keywords_top[2].id,
         data={'abbr': fake.word(), 'title': fake.company()},
         status='proposed',
         child_schema=None,
     )
-    r = client.post(f'/keyword/{keyword_1.key}', json=dict(
+    r = client.post(f'/keyword/{keyword_1.id}', json=dict(
         data=keyword_1.data,
+        parent_id=keyword_1.parent_id,
     ))
 
     if authorized:
         assert_json_result(r, r.json(), keyword_1)
 
         keyword_2 = keyword_build(
-            parent_key=keyword_1.key,
+            parent_id=keyword_1.id,
             data={'abbr': fake.word(), 'title': fake.company()},
             status='proposed',
             child_schema=None,
         )
-        r = client.post(f'/keyword/{keyword_2.key}', json=dict(
+        r = client.post(f'/keyword/{keyword_2.id}', json=dict(
             data=keyword_2.data,
+            parent_id=keyword_2.parent_id,
         ))
         assert_json_result(r, r.json(), keyword_2)
 
@@ -297,17 +301,19 @@ def test_suggest_keyword_parent_not_found(
 
     r = client.post('/keyword/foo/bar', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='foo',
     ))
     if authorized:
         assert_not_found(r, "Parent keyword 'foo' does not exist")
     else:
         assert_forbidden(r)
 
-    r = client.post(f'/keyword/{(key := keywords_top[2].key)}/foo/bar', json=dict(
+    r = client.post(f'/keyword/{(parent_id := keywords_top[2].id)}/foo/bar', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id=f'{parent_id}/foo',
     ))
     if authorized:
-        assert_not_found(r, f"Parent keyword '{key}/foo' does not exist")
+        assert_not_found(r, f"Parent keyword '{parent_id}/foo' does not exist")
     else:
         assert_forbidden(r)
 
@@ -316,21 +322,47 @@ def test_suggest_keyword_parent_not_found(
 
 
 @pytest.mark.require_scope(ODPScope.KEYWORD_SUGGEST)
-def test_suggest_keyword_no_parent(
+def test_suggest_keyword_invalid_parent(
         api,
         scopes,
         keyword_batch,
 ):
     keywords_top, keywords_flat = keyword_batch
     authorized = ODPScope.KEYWORD_SUGGEST in scopes
+    client = api(scopes)
 
-    r = api(scopes).post('/keyword/foo', json=dict(
+    # top-level keyword (vocab) cannot be created via API
+    r = client.post('/keyword/foo', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='',
     ))
     if authorized:
-        assert_unprocessable(r, "key must be suffixed to a parent key")
+        assert_unprocessable(r)
     else:
         assert_forbidden(r)
+
+    r = client.post('/keyword/foo/bar', json=dict(
+        data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='bar',
+    ))
+    if authorized:
+        assert_unprocessable(r, "'bar' cannot be a parent of 'foo/bar'")
+    else:
+        assert_forbidden(r)
+
+    # sibling cannot be parent
+    for n in range(1, 4):
+        for kw in reversed(keywords_flat):
+            if kw.id.count('/') == n:
+                r = client.post(f'/keyword/{kw.parent.id}/foo', json=dict(
+                    data={'abbr': fake.word(), 'title': fake.company()},
+                    parent_id=f'{kw.id}',
+                ))
+                if authorized:
+                    assert_unprocessable(r, f"'{kw.id}' cannot be a parent of '{kw.parent.id}/foo'")
+                else:
+                    assert_forbidden(r)
+                break
 
     assert_db_state(keywords_flat)
     assert_no_audit_log()
@@ -348,12 +380,13 @@ def test_suggest_keyword_conflict(
 
     for n in range(1, 4):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.post(f'/keyword/{kw.key}', json=dict(
+            if kw.id.count('/') == n:
+                r = client.post(f'/keyword/{kw.id}', json=dict(
                     data={'abbr': fake.word(), 'title': fake.company()},
+                    parent_id=kw.parent_id,
                 ))
                 if authorized:
-                    assert_conflict(r, f"Keyword '{kw.key}' already exists")
+                    assert_conflict(r, f"Keyword '{kw.id}' already exists")
                 else:
                     assert_forbidden(r)
                 break
@@ -371,8 +404,9 @@ def test_suggest_keyword_invalid_data(
     keywords_top, keywords_flat = keyword_batch
     authorized = ODPScope.KEYWORD_SUGGEST in scopes
 
-    r = api(scopes).post(f'/keyword/{keywords_top[2].key}/foo', json=dict(
-        data={'title': fake.company()},  # missing required property 'abbr'
+    r = api(scopes).post(f'/keyword/{keywords_top[2].id}/foo', json=dict(
+        data={'abbr': fake.word()},  # missing required property 'title'
+        parent_id=keywords_top[2].id,
     ))
     if authorized:
         assert_unprocessable(r, valid=False)
@@ -398,27 +432,28 @@ def test_set_keyword(
     for n in range(4):
         create = n == 0 or randint(0, 1)
         for i, kw in enumerate(keywords_flat):
-            if kw.key.count('/') == n:
+            if kw.id.count('/') == n:
                 data = {'abbr': fake.word(), 'title': fake.company()}
                 child_schema = FactorySession.execute(select(Schema)).scalars().first() if randint(0, 1) else None
 
                 if create:
                     kw_in = keyword_build(
-                        parent_key=kw.key,
+                        parent_id=kw.id,
                         data=data,
                         child_schema=child_schema,
                     )
                     audit += [dict(command='insert', keyword=kw_in)]
                 else:
                     kw_in = keyword_build(
-                        parent_key=kw.parent_key,
-                        key=kw.key,
+                        parent_id=kw.parent_id,
+                        id=kw.id,
                         data=data,
                         child_schema=child_schema,
                     )
                     audit += [dict(command='update', keyword=kw_in, replace=i)]
 
-                r = client.put(f'/keyword/{kw_in.key}', json=dict(
+                r = client.put(f'/keyword/{kw_in.id}', json=dict(
+                    parent_id=kw_in.parent_id,
                     data=kw_in.data,
                     status=kw_in.status,
                     child_schema_id=kw_in.child_schema_id,
@@ -454,17 +489,19 @@ def test_set_keyword_parent_not_found(
 
     r = client.put('/keyword/foo/bar', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='foo',
     ))
     if authorized:
         assert_not_found(r, "Parent keyword 'foo' does not exist")
     else:
         assert_forbidden(r)
 
-    r = client.put(f'/keyword/{(key := keywords_top[2].key)}/foo/bar', json=dict(
+    r = client.put(f'/keyword/{(parent_id := keywords_top[2].id)}/foo/bar', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id=f'{parent_id}/foo',
     ))
     if authorized:
-        assert_not_found(r, f"Parent keyword '{key}/foo' does not exist")
+        assert_not_found(r, f"Parent keyword '{parent_id}/foo' does not exist")
     else:
         assert_forbidden(r)
 
@@ -473,21 +510,51 @@ def test_set_keyword_parent_not_found(
 
 
 @pytest.mark.require_scope(ODPScope.KEYWORD_ADMIN)
-def test_set_keyword_no_parent(
+@pytest.mark.parametrize('create', [False, True])
+def test_set_keyword_invalid_parent(
         api,
         scopes,
         keyword_batch,
+        create,
 ):
     keywords_top, keywords_flat = keyword_batch
     authorized = ODPScope.KEYWORD_ADMIN in scopes
+    client = api(scopes)
 
-    r = api(scopes).put('/keyword/foo', json=dict(
+    # top-level keyword (vocab) cannot be created/updated via API
+    kw_id = 'foo' if create else keywords_top[0].id
+    r = client.put(f'/keyword/{kw_id}', json=dict(
         data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='',
     ))
     if authorized:
-        assert_unprocessable(r, "key must be suffixed to a parent key")
+        assert_unprocessable(r)
     else:
         assert_forbidden(r)
+
+    r = client.put(f'/keyword/{kw_id}/bar', json=dict(
+        data={'abbr': fake.word(), 'title': fake.company()},
+        parent_id='bar',
+    ))
+    if authorized:
+        assert_unprocessable(r, f"'bar' cannot be a parent of '{kw_id}/bar'")
+    else:
+        assert_forbidden(r)
+
+    # self or sibling cannot be parent
+    for n in range(1, 4):
+        for kw in reversed(keywords_flat):
+            if kw.id.count('/') == n:
+                kw_id = f'{kw.parent.id}/foo' if create else kw.id
+                r = client.put(f'/keyword/{kw_id}', json=dict(
+                    data={'abbr': fake.word(), 'title': fake.company()},
+                    parent_id=f'{kw.id}',
+                ))
+                if authorized:
+                    assert_unprocessable(r, f"'{kw.id}' cannot be a parent of '{kw_id}'")
+                else:
+                    assert_forbidden(r)
+                break
 
     assert_db_state(keywords_flat)
     assert_no_audit_log()
@@ -502,8 +569,9 @@ def test_set_keyword_invalid_data(
     keywords_top, keywords_flat = keyword_batch
     authorized = ODPScope.KEYWORD_ADMIN in scopes
 
-    r = api(scopes).put(f'/keyword/{keywords_top[2].key}/foo', json=dict(
+    r = api(scopes).put(f'/keyword/{keywords_top[2].id}/foo', json=dict(
         data={'abbr': fake.word()},  # missing required property 'title'
+        parent_id=keywords_top[2].id,
     ))
     if authorized:
         assert_unprocessable(r, valid=False)
@@ -528,17 +596,17 @@ def test_delete_keyword(
     audit = []
     for n in range(4):
         for i, kw in enumerate(keywords_flat):
-            if kw.key.count('/') == n:
+            if kw.id.count('/') == n:
                 if can_delete := not kw.children:
                     audit += [dict(command='delete', keyword=kw, index=i)]
 
-                r = client.delete(f'/keyword/{kw.key}')
+                r = client.delete(f'/keyword/{kw.id}')
 
                 if authorized:
                     if can_delete:
                         assert_empty_result(r)
                     else:
-                        assert_unprocessable(r, f"Keyword '{kw.key}' cannot be deleted as it has sub-keywords")
+                        assert_unprocessable(r, f"Keyword '{kw.id}' cannot be deleted as it has sub-keywords")
                 else:
                     assert_forbidden(r)
                 break
@@ -566,10 +634,10 @@ def test_delete_keyword_not_found(
 
     for n in range(2):
         for kw in reversed(keywords_flat):
-            if kw.key.count('/') == n:
-                r = client.delete(f'/keyword/{kw.key}/foo')
+            if kw.id.count('/') == n:
+                r = client.delete(f'/keyword/{kw.id}/foo')
                 if authorized:
-                    assert_not_found(r, f"Keyword '{kw.key}/foo' does not exist")
+                    assert_not_found(r, f"Keyword '{kw.id}/foo' does not exist")
                 else:
                     assert_forbidden(r)
                 break
