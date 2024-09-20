48f7c2d19f70af59f4e52e57fc22ba2a40ca0ed3
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2024-09-20 17:01:21 +0200

    Remove unused dependency function

--- odp/api/lib/schema.py
+++ odp/api/lib/schema.py
@@ -5,7 +5,7 @@ from jschon import JSON, JSONSchema, URI
 from sqlalchemy import select
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
-from odp.api.models import PackageModelIn, RecordModelIn, TagInstanceModelIn
+from odp.api.models import RecordModelIn, TagInstanceModelIn
 from odp.const.db import SchemaType
 from odp.db import Session
 from odp.db.models import Schema, Tag, Vocabulary
@@ -28,13 +28,6 @@ async def get_vocabulary_schema(vocabulary_id: str) -> JSONSchema:
         raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, 'Invalid vocabulary id')
 
     schema = Session.get(Schema, (vocabulary.schema_id, SchemaType.vocabulary))
-    return schema_catalog.get_schema(URI(schema.uri))
-
-
-async def get_package_schema(package_in: PackageModelIn) -> JSONSchema:
-    if not (schema := Session.get(Schema, (package_in.schema_id, SchemaType.metadata))):
-        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, 'Invalid schema id')
-
     return schema_catalog.get_schema(URI(schema.uri))
 
 
