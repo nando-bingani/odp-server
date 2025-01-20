e20e2f45269647454ccfa98011e6d2929f39aaa4
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2025-01-20 12:49:50 +0200

    Simplify vocabulary API
    There's no need to fetch associated keywords along with a vocabulary object - for that we have the keyword API.

--- odp/api/routers/vocabulary.py
+++ odp/api/routers/vocabulary.py
@@ -5,10 +5,8 @@ from starlette.status import HTTP_404_NOT_FOUND
 
 from odp.api.lib.auth import Authorize
 from odp.api.lib.paging import Paginator
-from odp.api.models import Page, VocabularyDetailModel, VocabularyModel
-from odp.api.routers.keyword import RecurseMode, output_keyword_model
+from odp.api.models import Page, VocabularyModel
 from odp.const import ODPScope
-from odp.const.db import KeywordStatus
 from odp.db import Session
 from odp.db.models import Vocabulary
 from odp.lib.schema import schema_catalog
@@ -16,30 +14,16 @@ from odp.lib.schema import schema_catalog
 router = APIRouter()
 
 
-def output_vocabulary_model(
-        vocabulary: Vocabulary,
-        detail=False,
-) -> VocabularyModel | VocabularyDetailModel:
-    cls = VocabularyDetailModel if detail else VocabularyModel
-    kwargs = dict(
+def output_vocabulary_model(vocabulary: Vocabulary) -> VocabularyModel:
+    return VocabularyModel(
         id=vocabulary.id,
         uri=vocabulary.uri,
         schema_id=vocabulary.schema_id,
         schema_uri=vocabulary.schema.uri,
+        schema_=schema_catalog.get_schema(URI(vocabulary.schema.uri)).value,
         static=vocabulary.static,
         keyword_count=len(vocabulary.keywords),
     )
-    if detail:
-        kwargs |= dict(
-            schema_=schema_catalog.get_schema(URI(vocabulary.schema.uri)).value,
-            keywords=[
-                output_keyword_model(keyword, recurse=RecurseMode.APPROVED)
-                for keyword in vocabulary.keywords
-                if keyword.parent_id is None and keyword.status == KeywordStatus.approved
-            ]
-        )
-
-    return cls(**kwargs)
 
 
 @router.get(
@@ -64,12 +48,11 @@ async def list_vocabularies(
 )
 async def get_vocabulary(
         vocabulary_id: str,
-) -> VocabularyDetailModel:
+) -> VocabularyModel:
     """
-    Get a vocabulary with its (approved) keywords, nested if hierarchical.
-    Requires scope `odp.vocabulary:read`.
+    Get a vocabulary. Requires scope `odp.vocabulary:read`.
     """
     if not (vocabulary := Session.get(Vocabulary, vocabulary_id)):
         raise HTTPException(HTTP_404_NOT_FOUND)
 
-    return output_vocabulary_model(vocabulary, detail=True)
+    return output_vocabulary_model(vocabulary)
--- test/api/test_vocabulary.py
+++ test/api/test_vocabulary.py
@@ -6,7 +6,6 @@ from sqlalchemy import select
 from odp.const import ODPScope
 from odp.db.models import Keyword, Vocabulary
 from test import TestSession
-from test.api import test_keyword
 from test.api.assertions import assert_forbidden, assert_not_found
 from test.factories import KeywordFactory, VocabularyFactory
 
@@ -35,29 +34,20 @@ def assert_db_state(vocabularies):
         assert row.static == vocabularies[n].static
 
 
-def assert_json_result(response, json, vocabulary, detail=False):
+def assert_json_result(response, json, vocabulary):
     """Verify that the API result matches the given vocabulary object."""
     assert response.status_code == 200
     assert json['id'] == vocabulary.id
     assert json['uri'] == vocabulary.uri
     assert json['schema_id'] == vocabulary.schema_id
     assert json['schema_uri'] == vocabulary.schema.uri
+    assert json['schema_']['$id'] == vocabulary.schema.uri
     assert json['static'] == vocabulary.static
 
-    if detail:
-        assert json['schema_']['$id'] == vocabulary.schema.uri
-        db_keywords = TestSession.execute(
-            select(Keyword).
-            where(Keyword.vocabulary_id == vocabulary.id).
-            where(Keyword.status == 'approved').
-            where(Keyword.parent_id == None)
-        ).scalars().all()
-        db_keywords.sort(key=lambda k: k.id)
-        json_keywords = json['keywords']
-        json_keywords.sort(key=lambda k: k['id'])
-        assert len(json_keywords) == len(db_keywords)
-        for n, json_keyword in enumerate(json_keywords):
-            test_keyword.assert_json_result(response, json_keyword, db_keywords[n], 'approved')
+    db_keywords = TestSession.execute(
+        select(Keyword).where(Keyword.vocabulary_id == vocabulary.id)
+    ).scalars().all()
+    assert json['keyword_count'] == len(db_keywords)
 
 
 def assert_json_results(response, json, vocabularies):
@@ -77,31 +67,35 @@ def test_list_vocabularies(
         scopes,
 ):
     authorized = ODPScope.VOCABULARY_READ in scopes
+
     r = api(scopes).get('/vocabulary/')
-    if authorized:
-        assert_json_results(r, r.json(), vocabulary_batch)
-    else:
+
+    if not authorized:
         assert_forbidden(r)
+    else:
+        assert_json_results(r, r.json(), vocabulary_batch)
+
     assert_db_state(vocabulary_batch)
 
 
 @pytest.mark.require_scope(ODPScope.VOCABULARY_READ)
+@pytest.mark.parametrize('error', [None, '404'])
 def test_get_vocabulary(
         api,
         vocabulary_batch,
         scopes,
+        error,
 ):
     authorized = ODPScope.VOCABULARY_READ in scopes
-    r = api(scopes).get(f'/vocabulary/{vocabulary_batch[2].id}')
-    if authorized:
-        assert_json_result(r, r.json(), vocabulary_batch[2], True)
-    else:
+    vocab_id = 'foo' if error == '404' else vocabulary_batch[2].id
+
+    r = api(scopes).get(f'/vocabulary/{vocab_id}')
+
+    if not authorized:
         assert_forbidden(r)
-    assert_db_state(vocabulary_batch)
+    elif error == '404':
+        assert_not_found(r)
+    else:
+        assert_json_result(r, r.json(), vocabulary_batch[2])
 
-
-def test_get_vocabulary_not_found(api, vocabulary_batch):
-    scopes = [ODPScope.VOCABULARY_READ]
-    r = api(scopes).get('/vocabulary/foo')
-    assert_not_found(r)
     assert_db_state(vocabulary_batch)
