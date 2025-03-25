5b8ad4a61b82003442d8ab171072dedf84ca204a
Mark Jacobson <52427991+marksparkza@users.noreply.github.com>
2025-03-25 13:54:04 +0200

    Use package key as the archive folder

--- odp/api/routers/archive.py
+++ odp/api/routers/archive.py
@@ -15,7 +15,7 @@ from odp.api.models import ArchiveModel, Page
 from odp.const import ODPScope
 from odp.const.db import HashAlgorithm
 from odp.db import Session
-from odp.db.models import Archive, ArchiveResource, Package, Provider, Resource
+from odp.db.models import Archive, ArchiveResource, Package, Resource
 
 router = APIRouter()
 
@@ -78,13 +78,12 @@ async def get_archive(
 
 
 @router.put(
-    '/{archive_id}/{provider_id}/{package_id}/{folder:path}',
+    '/{archive_id}/{package_key}/{folder:path}',
     dependencies=[Depends(ArchiveAuthorize())],
 )
 async def upload_file(
         archive_id: str,
-        provider_id: str,
-        package_id: str,
+        package_key: str,
         folder: str = Path(..., title='Path to containing folder relative to package root'),
         unpack: bool = Query(False, title='Unpack zip file into folder'),
         file: UploadFile = File(..., title='File upload'),
@@ -93,7 +92,6 @@ async def upload_file(
         title: str = Query(None, title='Resource title'),
         description: str = Query(None, title='Resource description'),
         archive_adapter: ArchiveAdapter = Depends(get_archive_adapter),
-        provider_auth: Authorized = Depends(Authorize(ODPScope.PROVIDER_READ)),
         package_auth: Authorized = Depends(Authorize(ODPScope.PACKAGE_WRITE)),
 ) -> None:
     """
@@ -105,67 +103,45 @@ async def upload_file(
     resource is created and similarly associated.
     """
     if not (archive := Session.get(Archive, archive_id)):
-        raise HTTPException(
-            HTTP_404_NOT_FOUND, 'Archive not found'
-        )
+        raise HTTPException(HTTP_404_NOT_FOUND, 'Archive not found')
 
-    provider_auth.enforce_constraint([provider_id])
-    if not (provider := Session.get(Provider, provider_id)):
-        raise HTTPException(
-            HTTP_404_NOT_FOUND, 'Provider not found'
-        )
+    if not (package := Session.execute(select(Package).where(Package.key == package_key)).scalar_one_or_none()):
+        raise HTTPException(HTTP_404_NOT_FOUND, 'Package not found')
 
-    if not (package := Session.get(Package, package_id)):
-        raise HTTPException(
-            HTTP_404_NOT_FOUND, 'Package not found'
-        )
     package_auth.enforce_constraint([package.provider_id])
 
     if '..' in folder:
-        raise HTTPException(
-            HTTP_422_UNPROCESSABLE_ENTITY, "'..' not allowed in folder"
-        )
+        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, "'..' not allowed in folder")
 
     if pathlib.Path(folder).is_absolute():
-        raise HTTPException(
-            HTTP_422_UNPROCESSABLE_ENTITY, 'folder must be relative'
-        )
+        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, 'folder must be relative')
 
     if not (filename := secure_filename(filename)):
-        raise HTTPException(
-            HTTP_422_UNPROCESSABLE_ENTITY, 'invalid filename'
-        )
+        raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, 'invalid filename')
 
-    archive_folder = f'{provider.key}/{package.key}/{folder}'
+    archive_folder = f'{package_key}/{folder}'
     try:
         file_info_list = await archive_adapter.put(
             archive_folder, filename, file, sha256, unpack
         )
     except NotImplementedError:
-        raise HTTPException(
-            HTTP_405_METHOD_NOT_ALLOWED, f'Operation not supported for {archive.id}'
-        )
+        raise HTTPException(HTTP_405_METHOD_NOT_ALLOWED, f'Operation not supported for {archive.id}')
 
     for file_info in file_info_list:
-        res_mimetype, encoding = mimetypes.guess_type(file_info.relpath, strict=False)
-        package_path = file_info.relpath.removeprefix(f'{provider.key}/{package.key}/')
         archive_path = file_info.relpath
-        res_filename = pathlib.Path(file_info.relpath).name
-        res_size = file_info.size
-        res_hash = file_info.sha256
-        res_title = title
-        res_description = description
+        package_path = file_info.relpath.removeprefix(f'{package_key}/')
 
         resource = Resource(
-            title=res_title,
-            description=res_description,
-            filename=res_filename,
-            mimetype=res_mimetype,
-            size=res_size,
-            hash=res_hash,
+            package_id=package.id,
+            folder=str(pathlib.Path(package_path).parent),
+            filename=pathlib.Path(file_info.relpath).name,
+            mimetype=mimetypes.guess_type(file_info.relpath, strict=False)[0],
+            size=file_info.size,
+            hash=file_info.sha256,
             hash_algorithm=HashAlgorithm.sha256,
+            title=title,
+            description=description,
             timestamp=(timestamp := datetime.now(timezone.utc)),
-            provider_id=provider.id,
         )
         resource.save()
 
@@ -180,17 +156,4 @@ async def upload_file(
         except IntegrityError:
             raise HTTPException(
                 HTTP_422_UNPROCESSABLE_ENTITY, f"Path '{archive_path}' already exists in archive"
-            )
-
-        try:
-            package_resource = PackageResource(
-                package_id=package.id,
-                resource_id=resource.id,
-                path=package_path,
-                timestamp=timestamp,
-            )
-            package_resource.save()
-        except IntegrityError:
-            raise HTTPException(
-                HTTP_422_UNPROCESSABLE_ENTITY, f"Path '{package_path}' already exists in package"
             )
